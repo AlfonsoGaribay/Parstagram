9f9df82ea9336d3b4d3974064ad43b889dfbb21d
Fonzie <alfonsogaribay89@yahoo.com>
2021-03-19 21:51:20 -0700

    this is the finished app, a feature that was not included, but that i want to add would be pulldown to reload the feed instead of logging back in to see new photos

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Fonzie on 3/19/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
