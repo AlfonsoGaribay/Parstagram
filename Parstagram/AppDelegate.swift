2e7ded539dd3804ac97b0cd6e25140738c9d45e9
Fonzie <alfonsogaribay89@yahoo.com>
2021-03-19 20:06:03 -0700

    This contains the pods to include parse, it is connected to a parse server that is now storing user profiles(names and passwords) also i can login and sign up, a segue if its valid an error if its not!

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // --- Copy this only
+            
+            let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "fKa4CO9aJtPtK73j2QGYD0EQrHcinNAjKwbkQnzm" // <- UPDATE
+                    $0.clientKey = "4lkUEv1CigvLXy5ATIH5A0Vt6yE09WIcAUUgYMtk" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
+            // --- end copy
+        
         return true
     }
 
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
